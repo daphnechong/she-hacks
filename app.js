18cbd1456d8def1294868927c35e4f3665c856cb
Daphne Chong <daphne.h.chong@gmail.com>
2014-03-21 21:26:12 +1100

    create basic root and admin handlebars pages

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello world');
+app.set('port', '3000');
+app.use(express.logger("dev"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.use(express.errorHandler());
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/admin', function(req, res) {
+  res.render('admin');
 });
 
 var server = app.listen(3000, function() {
