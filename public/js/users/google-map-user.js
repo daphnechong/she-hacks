c656b115d6b35c5f605142b745b9f94fae3335fb
Luciana Carrolo <luciana.carrolo@gmail.com>
2014-03-22 15:38:57 +1100

    User form.

--- public/js/users/google-map-user.js
+++ public/js/users/google-map-user.js
@@ -6,8 +6,35 @@ var setupListeners = function() {
 var points = [];
 var data = [];
 var forms = [];
+var musterLocation;
 
 function createMusterMarker(data) {
+
+    var contentString = '<div id="content" >'+
+    '<h4 id="firstHeading" class="firstHeading">Disaster Volunteering</h4>'+
+    '<div id="bodyContent">'+
+        '<form role="form">' +
+            '<h5 id="firstHeading" class="firstHeading">Event Name</h5>'+
+            '<div>' +
+                '<p>' + data.name + '</p>'+
+            '</div>' +
+            '<h5 id="firstHeading" class="firstHeading">Description</h5>'+
+            '<div>' +
+                '<p>' + data.description + '</p>'+
+            '</div>' +
+            '<div class="form-group">' +
+                '<input type="text" class="form-control" id="volunteer-name" placeholder="Volunteer name">' +
+            '</div>' +
+            '<div class="form-group">' +
+                '<input type="text" class="form-control" id="volunteer-phone-num" placeholder="Phone number">' +
+            '</div>' +
+            '<div class="form-group">' +
+                '<input type="text" class="form-control" id="num-volunteers-friends" placeholder="Number of friends">' +
+            '</div>' +
+            '<div class="btn btn-default" onclick="registerVolunteer();">Attend event</div>' +
+        '</form>' +
+    '</div>'+
+    '</div>';
   if (stopDrawing) {
 
   	latLng = new google.maps.LatLng(data.location.k, data.location.A); // <-- this line
@@ -19,28 +46,43 @@ function createMusterMarker(data) {
     muster.setIcon('img/muster-not-full.png')
 
     form = new google.maps.InfoWindow({
-        content: data.name + ' ' + data.organiser + ' ' + data.phone + ' ' + data.description  + ' ' +data.numvolunteers
+        //content: data.name + ' ' + data.organiser + ' ' + data.phone + ' ' + data.description  + ' ' +data.numvolunteers
+        content: contentString
     });
 
     forms[data.location] = form;
     points[data.location] = muster;
 
-    form.open(map,muster);
+    musterLocation = data.location;
+    // form.open(map,muster);
 
     google.maps.event.addListener(muster, 'click', function() {
     	forms[data.location].open(map, muster);
 
-    	socket.emit('registerVolunteer', { location: data.location, name: 'alice', phone: '12121', numvolunteers: 2})
+    	
     });
   }
 }
 
-function registerVolunteer(data) {
+function registerVolunteer() {
 
-    var form = forms[data.location];
+    var form = forms[musterLocation];
 
-    var content = ' new update! ' + data.currentSignup + form.content;
+    // var volName = document.getElementById('volunteer-name').value;
+    // var volPhone = document.getElementById('volunteer-phone-num').value;
+    // var volNumFriends = document.getElementById('num-volunteers-friends').value;
 
-	form.content = content;
-	form.open(map, points[data.location]);
+    var data = { location: musterLocation, name: "Daphne", phone: "0451588585", numvolunteers: "2"};
+    socket.emit('registerVolunteer', data);
+    console.log("muster location: "+ musterLocation);
+    
+    form.close();
+    musterLocation = '';
+
+    
+
+ //    var content = ' new update! ' + data.currentSignup + form.content;
+
+	// form.content = content;
+	// form.open(map, points[data.location]);
 }
